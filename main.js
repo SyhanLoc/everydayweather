63c2069ce16bfb66d2f249cbe9ac55a3f1bbd8a5
Syhan Loc <syhanloc@gmail.com>
2017-12-09 17:50:30 +0000

    finally got the temperature to show

--- main.js
+++ main.js
@@ -10,10 +10,10 @@ $(function(){
             var apiData;
             
             backgroundImg = [
-                "https://dsx.weather.com//util/image/w/thunderstormasthma.jpg?v=at&w=1280&h=720&api=7db9fe61-7414-47b5-9871-e17d87b8b6a0",
-                "https://www.eastbaytimes.com/wp-content/uploads/2016/10/sjm-morerain-1017-23.jpg?w=620",
-                "https://images.gmanews.tv/webpics/2015/09/640_rainfall_2015_09_14_11_23_49.jpg",
-                "https://cdn.cnn.com/cnnnext/dam/assets/170314160205-boston-snow-03142017-exlarge-169.jpg",
+                /*thunderstom*/"https://dsx.weather.com//util/image/w/thunderstormasthma.jpg?v=at&w=1280&h=720&api=7db9fe61-7414-47b5-9871-e17d87b8b6a0",
+                /*drizzle rain*/"https://www.eastbaytimes.com/wp-content/uploads/2016/10/sjm-morerain-1017-23.jpg?w=620",
+                /*rain*/"https://images.gmanews.tv/webpics/2015/09/640_rainfall_2015_09_14_11_23_49.jpg",
+                /*snow storm*/"http://i2.wp.com/www.bikinginmpls.com/wp-content/uploads/2015/11/winter.jpeg?resize=858%2C488",
                 "https://www.theaa.com/~/media/the-aa/article-summaries/driving-advice/fog.jpg?h=400&w=640&hash=685991001A7B12CF60994E0655CA3B7638BF5BBC&la=en",
                 "https://i0.wp.com/snehaschoice.com/wp-content/uploads/2016/03/clear-sky-on-a-sunny-day-1255-1366x768.jpg",
                 "http://allswalls.com/images/cloudy-sky-wallpaper-7.jpg",
@@ -37,17 +37,17 @@ $(function(){
                 if(C) return Math.round((F-32)*(5/9)) + "&deg; C";
                 return Math.round(F) + "&deg; F";
             }
-            
-            function render (data,C){
+            /*where it's going to display */
+            function render (data, C){
                 var currentWeather = data.weather[0].description;
                 var currentTemp = displayTemp(data.main.temp,C);/*displayTemp is looking for my degree and F, I can feed the information to function displayTemp by putting ()*/
                 var icon = data.weather[0].icon;
                 
-                
+            /*overwrite the html*/    
                 $("#currentTemp").html(currentTemp);
                 $("#currentWeather").html(currentWeather);
                 
-                var apiIcon = '"https://openweathermap.org/img/w/" + icon + ".png"'; /*get icon code and insert onto end of html + .png to get correnct img icon*/
+                var apiIcon = "https://openweathermap.org/img/w/" + icon + ".png"; /*get icon code and insert onto end of html + .png to get correnct img icon*/
                 $("#currentTemp").prepend("<img src =" + apiIcon + ">");
             }
             /* get JSON location from freegeoip, once it's done, run a function, take one argument called location*/
@@ -59,7 +59,8 @@ $(function(){
                         $("#latitude").html(location.latitude);
                         $("#longitude").html(location.longitude);
                         
-    /*concatnate a string to make it a variable. API and then a ? to say there is a data coming. Use imerial because we want farenheit. after api, create a functio with data*/
+    /*concatnate a string to make it a variable. 
+    I have to feed them witht he weather. API and then a ? to say there is a data coming. Use imerial because we want farenheit. after api, create a functio with data*/
             $.getJSON('https://api.openweathermap.org/data/2.5/weather?lat='+location.latitude+'&lon='+location.longitude+'&units=imperial&appid=b2ce7b897c33473165e45bfae65201f2', function(data)
             {
                 apiData=data;
